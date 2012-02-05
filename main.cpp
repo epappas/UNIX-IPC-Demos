5c28de5c8dd920f28a90719fa7314661eab5f07c
BagosGiAr <papas.evagelos@gmail.com>
2012-02-05 21:15:45 +0200

    First Commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Evangelos Pappas - Evalon.gr
+ *
+ * Created on 13 Νοέμβριος 2010, 12:54 μμ
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main( int argc, char** argv )
+{
+
+    return 0;
+}
+
